ceb19060262b3f41b9ce26c9a847d107e58b02f6
thanhhnguyen23 <thanh.h.nguyen23@gmail.com>
2022-05-12 22:26:49 -0400

    add form control via stepper

---
+++ src/app/messages/message-new/message-new.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-message-new',
+  templateUrl: './message-new.component.html',
+  styleUrls: ['./message-new.component.css']
+})
+export class MessageNewComponent implements OnInit {
+  isLinear = false;
+
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
+
+}
--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -3,15 +3,33 @@ import { CommonModule } from '@angular/common';
 
 import { MessagesRoutingModule } from './messages-routing.module';
 import { MessageListComponent } from './message-list/message-list.component';
+import { MessageNewComponent } from './message-new/message-new.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [
-    MessageListComponent
+    MessageListComponent,
+    MessageNewComponent
   ],
   imports: [
     CommonModule,
-    MessagesRoutingModule
+    MessagesRoutingModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatStepperModule,
+    MatIconModule
   ]
 })
 export class MessagesModule { }
